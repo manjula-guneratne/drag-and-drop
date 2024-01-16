44ffbb044476282e7ec0f8d852921c1352439f7e
Manjula <mguneratne3@gmail.com>
2024-01-16 16:27:25 -0500

    completed

--- script.js
+++ script.js
@@ -14,15 +14,28 @@ draggables.forEach( draggables => {
 containers.forEach(container => {
     container.addEventListener('dragover', e => {
         e.preventDefault()
+
+        const afterElement = getDragAfterElement(container, e.clientY)
         const draggable = document.querySelector('.dragging')
-        container.appendChild(draggable)
+        if(afterElement == null){
+            container.appendChild(draggable)
+        } else {
+            container.insertBefore(draggable, afterElement)
+        }
     })
 }) 
         
 function getDragAfterElement(container, y){
     const draggableElements = [...container.querySelectorAll('draggable:not(.dragging')]
 
-    draggableElements.reduce((closest, child) => {
+    return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect()
-    }, {offset: Number.POSITIVE_INFINITY})
+        const offset = y - box.top - box.height/2
+        if (offset < 0 && offset > closest.offset){
+            return { offset: offset, Element: child}
+        }
+        else{
+            return closest
+        }
+    }, {offset: Number.NEGATIVE_INFINITY}).Element
 }
